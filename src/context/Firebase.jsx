c3e3b9b38ab21f3b2529e2c1d2d49b7fba11d53e
SantajitPatra <business.santajitpatra@gmail.com>
2023-06-01 23:59:12 +0530

    feat : context

---
+++ src/context/Firebase.jsx
@@ -0,0 +1,32 @@
+import { createContext } from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getDatabase, set, ref } from "firebase/database";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAlqEEIJcpZKDMaSjjRNGKygCk185VsLKA",
+  authDomain: "fir-app-6887b.firebaseapp.com",
+  projectId: "fir-app-6887b",
+  storageBucket: "fir-app-6887b.appspot.com",
+  messagingSenderId: "360106724818",
+  appId: "1:360106724818:web:aa519de908f4c05f864c0b",
+  databaseURL: "lhttps://fir-app-6887b-default-rtdb.firebaseio.com/",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const firebaseAuth = getAuth(firebaseApp )
+const database = getDatabase()
+
+const FirebaseContext = createContext(null);
+
+export const FirebaseProvider = (props) => {
+    const signupUserWithEmailAndPassword = (email, password) => {
+        return createUserWithEmailAndPassword(firebaseAuth, email, password)
+    }
+  return 
+  // eslint-disable-next-line no-unreachable
+  <FirebaseContext.Provider value={{ signupUserWithEmailAndPassword }} >
+    {props.children}
+  </FirebaseContext.Provider>;
+};
